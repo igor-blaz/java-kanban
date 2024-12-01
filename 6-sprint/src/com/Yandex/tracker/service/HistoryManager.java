87a9d99ce0036bd2bd38cda81e566c31fefac2ad
igor-blaz <igorblaz666@gmail.com>
2024-12-01 15:38:41 +0300

    Добавил изменения в папку 6-sprint

---
+++ 6-sprint/src/com/Yandex/tracker/service/HistoryManager.java
@@ -0,0 +1,17 @@
+package com.Yandex.tracker.service;
+
+import com.Yandex.tracker.model.Epic;
+import com.Yandex.tracker.model.Subtask;
+import com.Yandex.tracker.model.Task;
+import java.util.List;
+import java.util.ArrayList;
+import com.Yandex.tracker.model.Subtask;
+
+import java.util.ArrayList;
+
+
+public interface HistoryManager {
+    void add(Task task);
+    void remove(int id);
+    List<Task> getHistory();
+}
